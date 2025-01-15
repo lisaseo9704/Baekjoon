f7c9eadd36022852b1b89a4bfbb697f5934e23d5
HyeRyeong(Lisa) Seo <lisaseo9704@gmail.com>
2025-01-15 09:47:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+input()  
+numbers = map(int, sys.stdin.read().strip().split())
+prime_count = sum(1 for num in numbers if is_prime(num))
+print(prime_count)
