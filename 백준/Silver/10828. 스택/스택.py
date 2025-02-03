ad825924d7c3655a4324c3734a5ae84189555f0a
HyeRyeong(Lisa) Seo <lisaseo9704@gmail.com>
2025-02-03 10:41:47 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+    N = int(input())
+    stack = []
+    
+    for _ in range(N):
+        command = input().split()
+        
+        if command[0] == 'push':
+            stack.append(int(command[1]))
+            
+        elif command[0] == 'pop':
+            if stack:
+                print(stack.pop())
+            else:
+                print(-1)
+                
+        elif command[0] == 'size':
+            print(len(stack))
+            
+        elif command[0] == 'empty':
+            print(1 if not stack else 0)
+            
+        elif command[0] == 'top':
+            if stack:
+                print(stack[-1])
+            else:
+                print(-1)
+
+if __name__ == "__main__":
+    main()
