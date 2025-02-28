90bfe7bb08a2fcd689aad3ab0fc7918a85b40c5e
HyeRyeong(Lisa) Seo <lisaseo9704@gmail.com>
2025-02-28 09:51:40 +0900

    [Silver III] Title: 소수 구하기, Time: 160 ms, Memory: 40084 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,17 @@
+import sys
+
+def find_primes(M, N):
+    is_prime = [True] * (N + 1)
+    is_prime[0] = is_prime[1] = False  
+    
+    for i in range(2, int(N**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, N + 1, i):
+                is_prime[j] = False
+    
+    for i in range(M, N + 1):
+        if is_prime[i]:
+            print(i)
+
+M, N = map(int, input().split())
+find_primes(M, N)
